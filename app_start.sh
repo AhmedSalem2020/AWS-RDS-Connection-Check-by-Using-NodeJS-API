9aba02d71c430090f4907a2faff86cf7737ce707
ahmed_salem2020 <ahmed.salem@einfochips.com>
2022-09-30 19:57:53 +0200

    changes in appspec.yaml file

--- app_start.sh
+++ app_start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "app_start"
 pwd
+echo "app_start"
+sudo chmod -R 777 /home/ec2-user/nodejs-app
 cd /home/ec2-user/nodejs-app
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
 npm i 
 # npm install pm2 -g
 # pm2 start index.js
