e6abbd84ad4d6cb393e538a66a2d8d54244be3b8
ahmed_salem2020 <ahmed.salem@einfochips.com>
2022-09-30 22:25:56 +0200

    changes in appspec.yaml file

--- app_start.sh
+++ app_start.sh
@@ -10,7 +10,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-npm install pm2 -g
 npm i 
 pm2 start index.js --name "my-api"
 pm2 restart all
--- before_install.sh
+++ before_install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 echo "before_install"
-
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/nodejs-app"
 if [ -d "$DIR" ]; then
