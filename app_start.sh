a4ba168f9714e101e33014ff4db6981a76164398
ahmed_salem2020 <ahmed.salem@einfochips.com>
2022-09-30 22:08:08 +0200

    changes in appspec.yaml file

--- app_start.sh
+++ app_start.sh
@@ -10,10 +10,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-
+npm install pm2 -g
 npm i 
 pm2 start index.js --name "my-api"
 pm2 restart all
-
-# npm install pm2 -g
-# pm2 start index.js
