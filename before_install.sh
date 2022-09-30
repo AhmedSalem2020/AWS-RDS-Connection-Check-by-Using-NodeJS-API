e69dbba9d72f2619cad7a835d16d80b874176ef3
ahmed_salem2020 <ahmed.salem@einfochips.com>
2022-09-30 19:57:20 +0200

    changes in appspec.yaml file

--- before_install.sh
+++ before_install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 echo "before_install"
-pwd
-cd /home/ec2-user/nodejs-app
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/nodejs-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
