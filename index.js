9dc21888cc0b6612820304817c02b8b1c22991db
ahmed_salem2020 <ahmed.salem@einfochips.com>
2022-10-02 21:58:13 +0200

    changes

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// Load the AWS SDK
+var AWS = require('aws-sdk'),
+    region = "us-east-2",
+    secretName = "mydb_secret",
+    secret,
+    decodedBinarySecret;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    region: region
+});
+var DB_URI = "";
+client.getSecretValue({SecretId: secretName}, function(err, data) {
+    if (err) {
+        console.log("error in geting secret");
+    }
+    else {
+        // Decrypts secret using the associated KMS key.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = JSON.parse(data.SecretString);
+            DB_URI = secret;
+            console.log(DB_URI);
+        } else {
+            console.log("error in secret");
+        }
+    }
+   
+    // Your code goes here. 
+});
+
 const mysql = require('mysql2');
 const env = require('dotenv').config();
 const express = require('express')
@@ -7,7 +38,7 @@ const PORT = process.env.PORT
 
 function connectToMysql() {
     mysqlConnection = mysql.createConnection({
-        uri: process.env.DB_URI,        // example => mysql://user:password@localhost:port/dbName
+        uri: DB_URI,        // example => mysql://user:password@localhost:port/dbName
     })
     mysqlConnection.connect((err) => {
         if (!err) {
